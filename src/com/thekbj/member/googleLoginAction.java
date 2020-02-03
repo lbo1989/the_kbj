178c4bd6d3d2d547e47e7028b08a8e20db19a7bf
SuperCom <SuperCom@DESKTOP-M97L5UF>
2020-02-03 22:39:04 +0900

    google login complete!!

---
+++ src/com/thekbj/member/googleLoginAction.java
@@ -0,0 +1,24 @@
+package com.thekbj.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.thekbj.comm.Action;
+import com.thekbj.comm.ForwardAction;
+
+public class googleLoginAction implements Action {
+
+	@Override
+	public ForwardAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ForwardAction forward = new ForwardAction();
+		forward.setForward(true);
+		forward.setUrl("/WEB-INF/loginAPI/google.html");
+		 
+		return forward;
+	}
+
+}
